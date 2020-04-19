0dbd38181208bb4c51884030da23c2e59b417b56
DXQ <794055465@qq.com>
2020-04-20 02:24:59 +0800

    日常练习

---
+++ 剑指offer练习/机器人的运动范围.cpp
@@ -0,0 +1,49 @@
+int movingCount(int threshold,int rows,int cols)
+{
+    if(threshold<0||rows<=0||cols<=0)
+        return 0;//健壮性
+
+    bool* visited=new bool[rows*cols];//标记数组 用指针控制
+    for(int i=0;i<rows*cols;i++)
+        visited[i]=false;//初始化
+
+    int count=movingCountCore(threshold,rows,cols,0,0,visited);
+
+    delete[] visited;//释放内存
+    return count;
+}
+
+int movingCountCore(int threshold,int rows,int cols,int row,int col,bool* visited)
+{
+    int count=0;
+    if(check(threshold,rows,cols,row,col,visited))
+    {
+        visited[row*cols+col]=true;//标记 不需要回溯
+
+        count=1+movingCountCore(threshold,rows,cols,row+1,col,visited)
+               +movingCountCore(threshold,rows,cols,row-1,col,visited)
+               +movingCountCore(threshold,rows,cols,row,col+1,visited)
+               +movingCountCore(threshold,rows,cols,row,col-1,visited);
+    }
+    return count;
+}
+
+bool check(int threshold,int rows,int cols,int row,int col,bool* visited)
+{
+    if(row>=0 && row<rows && col>=0 && col<cols && getDigitSum(row)+getDigitSum(col)<=threshold && !visited[row*cols+col])
+    {
+        return true;
+    }
+    return false;
+}
+
+int getDigitSum(int number)
+{
+    int sum=0;
+    while (number>0)
+    {
+        sum+=number%10;
+        number/=10;
+    }
+    return sum;
+}
