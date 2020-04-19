096c992e4c18a436fd28b1b7564ef0d84e68c1f2
DXQ <794055465@qq.com>
2020-04-19 15:46:14 +0800

    日常练习

--- 剑指offer练习/用两个栈实现队列.cpp
+++ 剑指offer练习/用两个栈实现队列.cpp
@@ -11,10 +11,10 @@ template<typename>class CQueue
     private:
         stack<T> stack1;//两个栈 泛型
         stack<T> stack2;
-}
+};
 
 template<typename T>void CQueue<T>::appendTail(const T& element)//重载
-{//插入队列时保证Stack2为空
+{//插入队列时保证Stack2为空 其实不必如此
     while(!stack2.empty())
     {
         //T& data;泛型变量定义
@@ -25,7 +25,7 @@ template<typename T>void CQueue<T>::appendTail(const T& element)//重载
 }
 
 template<typename T>T CQueue<T>::deleteHead()
-{//出队列时保证stack1为空
+{//出队列时保证stack1为空 其实不必如此
     while (!stack1.empty())
     {
         //T& data;泛型变量定义
@@ -41,4 +41,60 @@ template<typename T>T CQueue<T>::deleteHead()
     T head=stack2.top();//head是个地址
     stack2.pop();
     return head;
+}
+
+
+#include <iostream>
+#include <stack>
+#include <stdlib.h>
+using namespace std;
+ 
+template <typename T> class CQueue
+{
+public:
+	CQueue(void){};
+	~CQueue(void){};
+	void appendTail(const T& node);
+	T deleteHead();
+ 
+private:
+	stack<T> stack1;
+	stack<T> stack2;
+};
+ 
+template<typename T> 
+void CQueue<T>::appendTail(const T& node)
+{//插入只与stack1有关 
+	stack1.push(node);
+}
+ 
+template<typename T> T CQueue<T>::deleteHead()
+{//弹出只与stack2有关 若stack2为空就把stack1的元素灌进去
+	if(stack2.size() <= 0){
+		while(stack1.size() > 0){
+			T& data = stack1.top();//带不带&都可以
+			stack1.pop();
+			stack2.push(data);
+		}
+	}
+	if(stack2.size() == 0)
+		throw new exception("queue is empty");
+	T head = stack2.top();
+	stack2.pop();
+	return head;
+}
+int main()
+{
+	CQueue<int> qu;
+	int a = 1;
+	int b = 2;
+	int c = 3;
+	qu.appendTail(a);
+	qu.appendTail(b);
+	qu.appendTail(c);
+	cout<<qu.deleteHead()<<" ";
+	cout<<qu.deleteHead()<<" ";
+	cout<<qu.deleteHead()<<" ";
+	system("pause");
+	return 0;
 }
