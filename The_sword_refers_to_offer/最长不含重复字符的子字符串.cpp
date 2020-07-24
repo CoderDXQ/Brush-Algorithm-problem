d39573072f5c7d3b30bda79ff0973aab8d6d0b97
cococolabear <794055465@qq.com>
2020-07-25 02:14:22 +0800

    daily practice

--- The_sword_refers_to_offer/最长不含重复字符的子字符串.cpp
+++ The_sword_refers_to_offer/最长不含重复字符的子字符串.cpp
@@ -1,13 +1,31 @@
-//方法一：动态规划
+//方法一：动态规划+哈希技巧
+int longestSubstringWithoutDuplication(const std::string& str)
+{
+    int curLength=0;//片段长度
+    int maxLength=0;//
 
+    int* position=new int[26];
+    for(int i=0;i<str.length();i++)
+        position[i]=-1;//初始化
+    
+    for(int i=0;i<str.length();i++)
+    {
+        int prevIndex=position[str[i]-'a'];//上次出现这个字符的位置
+        if(prevIndex<0 || i-prevIndex>curLength)
+            curLength++;//这个字符头一次出现或者上一次出现在正在进行的片段之前
+        else
+        {//这个字符上次出现的位置在正在进行的片段内部，此时正在进行的片段完结
+            if(curLength>maxLength)//截取新的片段之前要判定存储最大长度
+                maxLength=curLength;//存储记录之后才能截取新的片段
 
-
-
-
-//方法二：标记+哈希
-
-找个编程网站测试一下
-
-遍历字符串，把遍历过的字符转换成数字，并以数字作为数组下标记录字符在字符串中的位置，
-当出现重复数字（在标记数组中的值不为-1）时，计算此时的不重复长度（用字符串下标计算），
-并与max进行比较，max存储最大长度，另外设计两个变量随max一起记录字符串的起始位置
+            curLength=i-prevIndex;//截取新的片段
+        }
+        position[str[i]-'a']=i;//更新某字符出现的最新位置
+    }
+    //这个步骤是补的，防止最后一个片段的长度是最大的而没有更新maxLength
+    if(curLength>maxLength)
+        maxLength=curLength;
+    
+    delete[] position;//清理内存
+    return maxLength;
+}
