d506e56299f40e7479d0386342e1fbb84b4c7a9f
cococolabear <794055465@qq.com>
2020-07-26 01:36:54 +0800

    daily practice

--- The_sword_refers_to_offer/第一个只出现一次的字符.cpp
+++ The_sword_refers_to_offer/第一个只出现一次的字符.cpp
@@ -1,6 +1,25 @@
 //字符串中第一个只出现一次的字符
+char FirstNotRepeatingChar(char* pString)
+{
+    if(pString==nullptr)
+        return '\0';//健壮性
+    const int tableSize=256;
+    unsigned int hashTable[tableSize];
+    for(unsigned int i=0;i<tableSize;i++)
+        hashTable[i]=0;//初始化
 
+    char* pHashKey=pString;
+    while(*(pHashKey)!='\0')
+        hashTable[*(pHashKey++)]++;//直接用ASCII码做下标
 
+    pHashKey=pString;//复位
+    while(*pHashKey!='\0')
+    {
+        if(hashTable[*pHashKey]==1)
+            return *pHashKey;//返回的是ASCII码可以直接输出为字符
 
+        pHashKey++;
+    }
 
-相关题目 题目二也要做
+    return '\0';
+}
