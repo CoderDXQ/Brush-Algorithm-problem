9ab7f36b5d536df45fdd3c110bd0458ea423a75c
cococolabear <794055465@qq.com>
2020-10-01 00:30:28 +0800

    daily practice

---
+++ 洛谷/P5706.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    double a;
+    int n;
+    cin>>a>>n;
+
+    printf("%.3lf\n%d\n",a/n,n*2);
+   // printf("%d\n",n*2);
+    return 0;
+}
