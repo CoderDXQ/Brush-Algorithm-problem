1f3396443f0223f2c27dbc2673d530aaac1e1dd2
cococolabear <794055465@qq.com>
2020-10-03 00:40:44 +0800

    daily practice

--- 洛谷/P1320.cpp
+++ 洛谷/P1320.cpp
@@ -1,4 +1,4 @@
-//这个题可以不存储直接输出
+// //这个题可以不存储直接输出
 #include<stdio.h>
 #include<iostream>
 #include<math.h>
@@ -13,10 +13,9 @@ int main()
     int sum,s;
     vector<int> sav;
 
-    cin>>a;
-    pre=a;
-    sum=1;
-    s=1;
+    pre='0';//题目规定第一个数是0的个数，01轮换
+    sum=0;
+    s=0;
 
     while(cin>>a)
     {
@@ -36,9 +35,41 @@ int main()
     sav.push_back(s);
 
     cout<<sqrt(sum);
-    for(vector<int>::iterator it=sav.begin();it<=sav.end();it++)
-        cout<<" "<<*it;
+    for(vector<int>::iterator it=sav.begin();it!=sav.end();it++)
+        printf(" %d",(int)(*it));//这里必须要进行强制类型转换  有些编译器无法识别这个指针
     
    // cout<<endl;
     return 0;
 }
+
+// #include<bits/stdc++.h>
+
+// using namespace std;
+
+// int main()
+// {
+//     char a,b='0';//题目规定第一个数是0的个数
+//     int ans[1000000],n=0,zz=1;
+
+//     memset(ans,0,sizeof(ans));
+
+//     while(cin>>a)
+//     {
+//         if(a==b)
+//         {
+//             ans[zz]++;
+//         }
+//         else
+//         {
+//             ans[++zz]++;
+//             b=a;
+//         }
+//         n++;
+//     }
+//     cout<<sqrt(n);
+
+//     for(int i=1;i<=zz;i++)
+//         cout<<" "<<ans[i];
+
+//     return 0;
+// }
