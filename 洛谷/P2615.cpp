cb848dae862d5f2fec6a292af13ec9d0f36c3d1f
cococolabear <794055465@qq.com>
2020-10-05 23:17:26 +0800

    daily practice

--- 洛谷/P2615.cpp
+++ 洛谷/P2615.cpp
@@ -83,14 +83,14 @@ int main()
     x=1,y=(n+1)/2;
 
     for(int i=1;i<=n*n;i++)
-    {
+    {//这个幻方的规则就是如果右上为空就填上，否则就填在正下方 如果到头，按滚动的情况处理
         a[x][y]=i;
-        if(!a[(x-2+n)%n+1][y%n+1]) x=(x-2+n)%n+1,y=y%n+1;
+        if(!a[(x-2+n)%n+1][y%n+1]) x=(x-2+n)%n+1,y=y%n+1;//从第1行跳转到第n行的技巧  remember！！！
         else x=x%n+1;
     }
 
     for(int i=1;i<=n;i++)
-    {
+    { 
         for(int j=1;j<=n;j++)
             cout<<a[i][j]<<" ";
         cout<<endl;
