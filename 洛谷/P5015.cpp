93a9e4928cc0bd9a8fa1ac3e393af2f82e54366f
cococolabear <794055465@qq.com>
2020-10-06 23:09:26 +0800

    daily practice

--- 洛谷/P5015.cpp
+++ 洛谷/P5015.cpp
@@ -1,3 +1,30 @@
+// #include<stdio.h>
+// #include<iostream>
+// #include<istream>
+
+// using namespace std;
+
+// int main()
+// {
+//     string st;
+//     getline(cin,st);
+
+//     int len,sum=0;
+//     len=st.length();
+//     for(int i=0;i<len;i++)
+//     {
+//         if(st[i]!=' ')
+//             sum++;
+//     }
+
+//     cout<<sum<<endl;
+//   //  cout<<st<<endl;
+
+
+
+//     return 0;
+// }
+
 #include<stdio.h>
 #include<iostream>
 #include<istream>
@@ -7,18 +34,15 @@ using namespace std;
 int main()
 {
     string st;
-    getline(cin,st);
 
     int len,sum=0;
-    len=st.length();
-    for(int i=0;i<len;i++)
-    {
-        if(st[i]!=' ')
-            sum++;
-    }
 
+    while(cin>>st)//在测试环境中可以 但是在本地不好测试
+    {
+        sum+=st.length();
+    }
     cout<<sum<<endl;
-    cout<<st<<endl;
+  //  cout<<st<<endl;
 
 
 
