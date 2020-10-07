f505e9aeabc07c1de934c99af49bc801b120a620
cococolabear <794055465@qq.com>
2020-10-08 00:47:14 +0800

    daily practice

--- 洛谷/test.cpp
+++ 洛谷/test.cpp
@@ -7,12 +7,19 @@ using namespace std;
 
 int main()
 {
-    vector<int> sav;
-    sav.push_back(1);
-    sav.push_back(2);
-    for(vector<int>::iterator it=sav.begin();it!=sav.end();it++)
-        cout<<" "<<*it;
+    // vector<int> sav;
+    // sav.push_back(1);
+    // sav.push_back(2);
+    // for(vector<int>::iterator it=sav.begin();it!=sav.end();it++)
+    //     cout<<" "<<*it;
     
-    cout<<endl;
+    // cout<<endl;
+
+
+    string st;
+    getline(cin,st);
+    for(int i=0;i<=st.length();i++) //输出所有字符包括末尾回车的ASCII码   getline方法会读入末尾的换行符
+        cout<<(int)(st[i])<<" ";
+
     return 0;
 }
