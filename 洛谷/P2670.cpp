9ab62c9f17b87571caaa33f67430a67b720a92d0
cococolabear <794055465@qq.com>
2020-10-16 23:32:00 +0800

    daily practice

--- 洛谷/P2670.cpp
+++ 洛谷/P2670.cpp
@@ -1,3 +1,6 @@
+// 
+
+//思维简化
 #include <stdio.h>
 #include <iostream>
 
@@ -6,10 +9,8 @@ using namespace std;
 int main()
 {
     int n, m, x;
-    int matrix[105][105];
     int record[105][105];
     char ch;
-    memset(matrix, 0, sizeof(matrix)); 
     memset(record, 0, sizeof(record));
     cin >> n >> m;
 
@@ -21,24 +22,16 @@ int main()
         }
 
     for(int i=1;i<=n;i++)
-        for(int j=1;j<=m;j++)
-        {
-            if(record[i][j]!=1){
-                matrix[i][j]=record[i-1][j]+record[i+1][j]+record[i][j-1]+record[i][j+1]+record[i-1][j-1]+record[i-1][j+1]+record[i+1][j+1]+record[i+1][j-1];
-            }
-        }
-
-    for(int i=1;i<=n;i++)
     {
         for(int j=1;j<=m;j++)
-        {
+        {//直接计算输出，不存结果
             if(record[i][j]==1)
             {
                 cout<<"*";
             }
             else
             {
-                cout<<matrix[i][j];
+                cout<<record[i-1][j]+record[i+1][j]+record[i][j-1]+record[i][j+1]+record[i-1][j-1]+record[i-1][j+1]+record[i+1][j+1]+record[i+1][j-1];
             }
         }
         cout<<endl;
