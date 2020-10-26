ea46dbb160ca1332c6fbfd0a81480c43a879f11f
cococolabear <794055465@qq.com>
2020-10-26 18:48:54 +0800

    daily practice

--- 洛谷/test.cpp
+++ 洛谷/test.cpp
@@ -1,27 +1,35 @@
-#include<stdio.h>
-#include<iostream>
-#include<math.h>
-#include<vector>
+// #include<stdio.h>
+// #include<iostream>
+// #include<math.h>
+// #include<vector>
 
+// using namespace std;
+
+// int main()
+// {
+//     // vector<int> sav;
+//     // sav.push_back(1);
+//     // sav.push_back(2);
+//     // for(vector<int>::iterator it=sav.begin();it!=sav.end();it++)
+//     //     cout<<" "<<*it;
+    
+//     // cout<<endl;
+
+
+//     // string st;
+//     // getline(cin,st);
+//     // for(int i=0;i<=st.length();i++) //输出所有字符包括末尾回车的ASCII码   getline方法会读入末尾的换行符
+//     //     cout<<(int)(st[i])<<" ";
+
+//     // return 0;
+
+//     cout<<"\"";
+// }
+
+#include<bits/stdc++.h>
 using namespace std;
-
 int main()
 {
-    // vector<int> sav;
-    // sav.push_back(1);
-    // sav.push_back(2);
-    // for(vector<int>::iterator it=sav.begin();it!=sav.end();it++)
-    //     cout<<" "<<*it;
-    
-    // cout<<endl;
+    cout<<"hello_world"<<endl;
 
-
-    // string st;
-    // getline(cin,st);
-    // for(int i=0;i<=st.length();i++) //输出所有字符包括末尾回车的ASCII码   getline方法会读入末尾的换行符
-    //     cout<<(int)(st[i])<<" ";
-
-    // return 0;
-
-    cout<<"\"";
 }
